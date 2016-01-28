16ad275e543cf63a382afa3103db992fd5acd72a
Atte Tarvainen <atte.tarvainen@pp1.inet.fi>
2016-01-28 12:46:42 +0200

    Improve message sending options.

--- index.js
+++ index.js
@@ -5,18 +5,29 @@ exports = module.exports = (function () {
 	function Slackman (opts) {
 		this.hook = opts.hook || '';
 		this.debug = opts.debug || false;
+		this.username = opts.username || 'Slackman';
+		this.message = opts.message || 'Hello! I am the Slackman!';
+		this.channel = opts.channel || '#general';
 	}
 
-	Slackman.prototype.send = function (msg) {
+	Slackman.prototype.sendMessage = function (opts) {
+		var msg = opts && opts.message ? opts.message : this.message;
+		var channel = opts && opts.channel ? opts.channel : this.channel;
+		var username = opts && opts.username ? opts.username : this.username;
+
+		this.debug ? console.log('Sending message ' + msg + ' to channel ' + channel) : 0;
+
 		var that = this;
-		this.debug ? console.log('Sending message ' + msg) : 0;
+
 		request({
 			method: 'POST',
 			json: true,
 			url: that.hook,
 			headers: { 'Content-Type': 'application/json' },
 			body: {
-				text: msg || ''
+				text: msg,
+				channel: channel,
+				username: username,
 			}
 		});
 	}
